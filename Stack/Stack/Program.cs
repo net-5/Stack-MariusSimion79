109cdeb60c33c0c26e1080a21afd0a5f663f6aca
MariusSimion79 <mariussmn79@gmail.com>
2019-07-03 14:07:43 +0300

    Version1

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reach the last element of the stack without destroying the stack
+
+
+            Stack<int> myStack = new Stack<int>();
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+            myStack.Push(5);
+            Console.WriteLine("Initially, the stack is: ");
+            foreach (var elem in myStack)
+            {
+                Console.WriteLine(elem);
+            }
+
+            var last = myStack.Last();
+            Console.WriteLine();
+            Console.WriteLine($"The last element of the stack is: {last}");
+            Console.WriteLine();
+            Console.WriteLine("After viewing the last element, the stack remain: ");
+            foreach (var elem in myStack)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
